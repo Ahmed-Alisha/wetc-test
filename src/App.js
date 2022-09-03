c62d37acaaecb5129c7bd18536baf2ecc4cbcc39
Syed Ahmed Alisha <64783187+Ahmed-Alisha@users.noreply.github.com>
2022-09-04 03:06:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import { useEffect, useRef } from 'react'
+
+
+
+function App() {
+
+
+  const localVideoRef = useRef()
+  const remoteVideoRef = useRef()
+  const pc = useRef(new RTCPeerConnection(null))
+  const textRef = useRef()
+
+
+
+  useEffect(() => {
+    const constraints = {
+      audio: false,
+      video: true,
+      }
+
+    navigator.mediaDevices.getUserMedia(constraints)
+
+    .then(stream => {
+    localVideoRef.current.srcObject = stream
+
+    stream.getTracks().forEach(track => {
+    _pc.addTrack(track, stream)
+    })    
+    }) // display video
+
+    .catch(e => {console.log('getUserMedia Error ...', e)})
+
+    const _pc = new RTCPeerConnection(null)
+    _pc.onicecandidate = (e) => {
+      if (e.candidate)
+        console.log(JSON.stringify(e.candidate))
+    }
+
+    _pc.oniceconnectionstatechange = (e) => {
+      console.log(e)
+    }
+
+    _pc.ontrack = (e) => {
+      remoteVideoRef.current.srcObject = e.streams[0]
+    }
+
+    pc.current = _pc
+
+  }, [])
+
+
+
+  const createOffer = () => {
+    pc.current.createOffer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVideo: 1,
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp))
+      pc.current.setLocalDescription(sdp)
+    }).catch( e => console.log(e))
+  }
+
+  const createAnswer = () => {
+    pc.current.createAnswer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVideo: 1,
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp))
+      pc.current.setLocalDescription(sdp)
+    }).catch( e => console.log(e))
+  }
+
+
+  const setRemoteDescription = () => {
+    const sdp = JSON.parse(textRef.current.value)
+    console.log(sdp)
+
+    pc.current.setRemoteDescription(new RTCSessionDescription(sdp))
+  }
+  const addCandidate = () => {
+    const candidate = JSON.parse(textRef.current.value)
+    console.log('Adding Candiate', candidate)
+
+    pc.current.addIceCandidate(new RTCIceCandidate(candidate))
+  }
+
+
+
+
+
+  return (
+    <div style={{ margin: 10 }}>
+      <video 
+            style={{
+                    width: 854,height: 480, 
+                    margin: 5, backgroundColor: 'black'
+                  }} ref={localVideoRef} autoPlay> </video>
+
+      <video 
+            style={{
+                    width: 854,height: 480, 
+                    margin: 5, backgroundColor: 'black'
+                  }} ref={remoteVideoRef} autoPlay> </video>
+      <br />
+      <button onClick={ createOffer }> Create Offer </button>
+      <button onClick={ createAnswer }> Create Answer </button>
+      <br />
+      <textarea ref={textRef}></textarea>
+      <br />
+      <button onClick={ setRemoteDescription }> Set Remote Description </button>
+      <button onClick={ addCandidate }> Add Candidates </button>
+
+      
+    </div>
+  );
+}
+
+export default App;
